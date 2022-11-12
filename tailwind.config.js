814d86671dced9c7e536c9dc5cb9dc7e2a37130e
Eduardo Lago <laguillo@gmail.com>
2022-11-12 13:36:03 -0500

    Vistas de login y registro

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,9 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
     "./node_modules/flowbite/**/*.js",
   ],
+  theme: {
+    extend: {
+    },
+  },
   plugins: [require("flowbite/plugin")],
 };
