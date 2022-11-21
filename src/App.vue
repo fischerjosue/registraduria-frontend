de942b3877d6f7442c385b09a9076c1c1cbf9176
cvmesa <cvmesa29@gmail.com>
2022-11-20 23:08:56 -0500

    Imagenes en carpetas

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,11 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header class="flex items-center justify-between py-2 px-4 border-b">
-    <RouterLink to="/"><img class="w-28" src="@/assets/logo.png" alt="Registraduría Nacional del Estado Civil"/></RouterLink>
+    <RouterLink to="/"><img class="w-28" src="@/assets/images/logo.png" alt="Registraduría Nacional del Estado Civil"/></RouterLink>
     <nav class="flex gap-4 font-semibold">
       <RouterLink class="hover:text-gray-600 focus:text-black" to="/">Inicio</RouterLink>
       <RouterLink class="hover:text-gray-600 focus:text-black" to="/about">Nosotros</RouterLink>
-      <RouterLink class="hover:text-gray-600 focus:text-black" to="/login">Elecciones</RouterLink>
+      <RouterLink class="hover:text-gray-600 focus:text-black" to="/login">Iniciar sesión</RouterLink>
     </nav>
   </header>
 
