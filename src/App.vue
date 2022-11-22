b64ef9070d536c982dd3cf5d5beca41b6b5d0f63
cvmesa <cvmesa29@gmail.com>
2022-11-22 10:23:48 -0500

    .

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <template>
-
-
   <header class="flex items-center justify-between z-50 py-2 px-4 border-b bg-white">
     <RouterLink to="/"><img class="w-28" src="@/assets/logo.png" alt="Registraduría Nacional del Estado Civil" />
     </RouterLink>
