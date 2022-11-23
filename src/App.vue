ac379b9de05f4c7e8e8e0750101fcc5e13c2c54f
cvmesa <cvmesa29@gmail.com>
2022-11-22 22:57:55 -0500

    Revert

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <template>
+
+
   <header class="flex items-center justify-between z-50 py-2 px-4 border-b bg-white">
     <RouterLink to="/"><img class="w-28" src="@/assets/logo.png" alt="Registraduría Nacional del Estado Civil" />
     </RouterLink>
--- src/views/Elecciones/user/Dashboard.vue
+++ src/views/Elecciones/user/Dashboard.vue
@@ -1,21 +1,60 @@
 <template>
-    <main class="grid grid-cols-6 gap-4 mt-4 h-[calc(100vh-100px)]">
+    
+    <main class="grid grid-cols-6 mt-4 h-[calc(100vh-100px)]">
         <aside class="col-span-1  border-r ">
-            <nav class="grid">
-                <router-link to="">Resultado</router-link>
-                <router-link to="">Candidatos</router-link>
-                <router-link to="">Mesas</router-link>
-
+            <nav class="flex-col">
+                <a href="#">Partidos inscritos</a> 
+                <br>
+                <a href="#">Candidatos</a> 
+                <br>
+                <a href="#">Resulados</a>
+                
             </nav>
         </aside>
-
-        <section class="col-span-5">
-            <Candidatos />
-        </section>
     </main>
 
+    <div class="overflow-x-auto mt-4 relative shadow-sm sm:rounded-lg">
+        <table class="w-full text-xs md:text-sm text-center text-gray-500 dark:text-gray-400">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" class="py-3 px-6">
+                #
+              </th>
+              <th scope="col" class="py-3 px-6">
+                Nombre
+              </th>
+              <th scope="col" class="py-3 px-6">
+                Documento
+              </th>
+              <th scope="col" class="py-3 px-6">
+                Resolución
+              </th>
+              <th scope="col" class="py-3 px-6">
+                Partido
+              </th>
+            </tr>
+          </thead>
+          </table>
+          </div>
+
+
+    
 </template>
 
 <script setup>
-import Candidatos from '@/components/Candidatos.vue'
+import axios from 'axios'
+import { onMounted, ref } from 'vue';
+
+const items = ref([])
+onMounted(async () => {
+  await axios
+    .get('http://127.0.0.1:9999/candidatos')
+    .then(response => {
+      items.value = response.data
+      console.log(items)
+    })
+  Cookies.get('userLogged')
+    console.log(Cookies.value)
+})
 </script>
+
